743c9dd2ea1a8d22d65a2e5d755e17cf7bf0a319
KayraYavuz <ahmetkayrayavuz@gmail.com>
2026-01-10 16:15:03 +0300

    fix: Update import in widget_test.dart

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:ttlock_flutter_example/main.dart';
+import 'package:yavuz_lock/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
